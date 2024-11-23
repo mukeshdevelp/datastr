23fc74fbcb53f42e5fd2414411b858e201965533
mukesh <msmukeshkumarsharma95@gmail.com>
2024-11-23 21:23:06 +0530

    repo with Linked List added

---
+++ Linked list/SSLMain.java
@@ -0,0 +1,27 @@
+//Linked List
+
+class LinkedList {
+    Node head;
+    Node tail;
+
+    //No need to create an Object
+    static class Node{
+        int data;
+        Node next;
+        //next is default null
+        public Node(int d){
+            data = d;
+
+        }
+        
+    }    
+}
+
+
+
+public class SSLMain {
+
+    public static void main(String[] args) {
+
+    }
+}
